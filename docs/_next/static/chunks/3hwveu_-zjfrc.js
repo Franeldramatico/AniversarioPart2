f8a868b62d7e0d311a59930bf4429b08c1549f6f
fran <familiazv2016@gmail.com>
2026-06-08 00:01:40 -0400

    Preparar despliegue en carpeta docs para GitHub Pages

---
+++ docs/_next/static/chunks/3hwveu_-zjfrc.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,37292,t=>{t.v(o=>Promise.all(["static/chunks/26o5ftm0fjs31.js"].map(o=>t.l(o))).then(()=>o(99832)))}]);
